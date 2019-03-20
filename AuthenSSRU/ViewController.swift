5c94a9e755ff47037ce061dca05fbf379e34df71
aom <aomiris1998@gmail.com>
2019-03-20 15:13:09 +0700

    Back Process Success

--- AuthenSSRU/ViewController.swift
+++ AuthenSSRU/ViewController.swift
@@ -6,16 +6,10 @@
 //  Copyright © 2562 janejira. All rights reserved.
 //
 
-
-
-
 import UIKit
 
 class ViewController: UIViewController {
 
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
